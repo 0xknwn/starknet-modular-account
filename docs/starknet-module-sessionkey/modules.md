b76bb78aa843ce964124b1969ab22ae685561d0d
0xknwn <145777008+0xknwn@users.noreply.github.com>
2024-04-21 04:31:20 +0200

    fix the doc

---
+++ docs/starknet-module-sessionkey/modules.md
@@ -0,0 +1,24 @@
+[@0xknwn/starknet-module-sessionkey](README.md) / Exports
+
+# @0xknwn/starknet-module-sessionkey
+
+## Table of contents
+
+### Classes
+
+- [SessionKeyGrantor](classes/SessionKeyGrantor.md)
+- [SessionKeyModule](classes/SessionKeyModule.md)
+
+### Variables
+
+- [\_\_module\_validate\_\_](modules.md#__module_validate__)
+
+## Variables
+
+### \_\_module\_validate\_\_
+
+• `Const` **\_\_module\_validate\_\_**: ``"0x119c88dea7ff05dbe71c36247fc6682116f6dafa24089373d49aca7b2657017"``
+
+#### Defined in
+
+[sdks/starknet-module-sessionkey/src/sessionkey_validator.ts:10](https://github.com/0xknwn/starknet-modular-account/blob/4dae5b840adf2ab0ede919b63cafd321abb6f27e/sdks/starknet-module-sessionkey/src/sessionkey_validator.ts#L10)
---
+++ sdks/starknet-module-sessionkey/src/sessionkey_validator.test.ts
@@ -0,0 +1,278 @@
+import {
+  declareClass,
+  classHash,
+  deployCounter,
+  testAccounts,
+  default_timeout,
+  Counter,
+  counterAddress,
+  config,
+} from "starknet-test-helpers";
+
+describe("sessionkey management", () => {
+  let env: string;
+  let counterContract: Counter;
+
+  beforeAll(() => {
+    env = "devnet";
+  });
+
+  it(
+    "declare the Counter class",
+    async () => {
+      const conf = config(env);
+      const account = testAccounts(conf)[0];
+      const c = await declareClass(account, "Counter");
+      expect(c.classHash).toEqual(classHash("Counter"));
+    },
+    default_timeout
+  );
+
+  // it(
+  //   "gets the chain id",
+  //   async () => {
+  //     const conf = config(env);
+  //     connectedChain = await chain(conf.providerURL);
+  //     switch (env) {
+  //       case "sepolia":
+  //         expect(connectedChain).toBe("0x534e5f5345504f4c4941");
+  //         break;
+  //       default:
+  //         expect(connectedChain).toBe("0x534e5f5345504f4c4941");
+  //         break;
+  //     }
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "deploys the Counter contract",
+  //   async () => {
+  //     const a = testAccounts[0];
+  //     const c = await deployCounterContract(a);
+  //     expect(c.address).toEqual(counterAddress(a.address));
+  //     counterContract = {
+  //       classHash: classHash("Counter"),
+  //       address: counterAddress(a.address),
+  //     };
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "deploys the CoreValidator class",
+  //   async () => {
+  //     const a = testAccounts[0];
+  //     const c = await deployClass(a, "CoreValidator");
+  //     expect(c.classHash).toEqual(classHash("CoreValidator"));
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "deploys the Account class",
+  //   async () => {
+  //     const a = testAccounts[0];
+  //     const c = await deployClass(a, "SmartrAccount");
+  //     expect(c.classHash).toEqual(classHash("SmartrAccount"));
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "deploys the account contract",
+  //   async () => {
+  //     const a = testAccounts[0];
+  //     const publicKey = await testAccounts[0].signer.getPubKey();
+  //     const c = await deployAccount(a, "SmartrAccount", publicKey);
+  //     expect(c).toEqual(accountAddress("SmartrAccount", publicKey));
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "create and save the regular Multisig (no module)",
+  //   async () => {
+  //     const conf = config(env);
+  //     const p = provider(conf.providerURL);
+  //     const a = new Multisig(p, targetAccountConfigs[0].address, [
+  //       targetAccountConfigs[0].privateKey,
+  //     ]);
+  //     targetAccounts = [a];
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "checks the account public keys",
+  //   async () => {
+  //     const a = targetAccounts[0];
+  //     const c = await get_public_keys(a);
+  //     expect(Array.isArray(c)).toBe(true);
+  //     expect(c.length).toEqual(1);
+  //     expect(`0x${c[0].toString(16)}`).toEqual(
+  //       targetAccountConfigs[0].publicKey
+  //     );
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "deploys the SessionKeyValidator class",
+  //   async () => {
+  //     const a = testAccounts[0];
+  //     const c = await deployClass(a, "SessionKeyValidator");
+  //     expect(c.classHash).toEqual(classHash("SessionKeyValidator"));
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "adds a module to the account",
+  //   async () => {
+  //     const a = targetAccounts[0];
+  //     const c = await add_module(a, classHash("SessionKeyValidator"));
+  //     expect(c.isSuccess()).toEqual(true);
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "checks the module with the account",
+  //   async () => {
+  //     const a = targetAccounts[0];
+  //     const value = await is_module(a, classHash("SessionKeyValidator"));
+  //     expect(value).toBe(true);
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "resets the counter with owner",
+  //   async () => {
+  //     const a = testAccounts[0];
+  //     await reset(a, counterContract.address);
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "creates a typescript session key module",
+  //   async () => {
+  //     const conf = config(env);
+  //     const p = provider(conf.providerURL);
+  //     const module = new SessionKeyModule(
+  //       targetAccountConfigs[0].publicKey,
+  //       targetAccountConfigs[0].address,
+  //       classHash("SessionKeyValidator"),
+  //       connectedChain
+  //     );
+  //     sessionKeyModule = module;
+  //     let r = await module.request(classHash("CoreValidator"));
+  //     if (!connectedChain) {
+  //       expect(connectedChain).toBeDefined();
+  //       return;
+  //     }
+  //     expect(r.hash).toBe(
+  //       hash_auth_message(
+  //         targetAccountConfigs[0].address,
+  //         classHash("SessionKeyValidator"),
+  //         classHash("CoreValidator"),
+  //         targetAccountConfigs[0].publicKey,
+  //         "0x0",
+  //         "0x0",
+  //         connectedChain
+  //       )
+  //     );
+  //   },
+  //   timeout
+  // );
+
+  // it("signs the typescript session key module", async () => {
+  //   if (!sessionKeyModule) {
+  //     expect(sessionKeyModule).toBeDefined();
+  //     return;
+  //   }
+  //   let grantor = new SessionKeyGrantor(
+  //     classHash("CoreValidator"),
+  //     targetAccountConfigs[0].privateKey
+  //   );
+  //   let signature = await grantor.sign(sessionKeyModule);
+  //   expect(signature.length).toEqual(2);
+  //   sessionKeyModule.add_signature(signature);
+  // });
+
+  // it(
+  //   "resets and read the counter",
+  //   async () => {
+  //     const a = testAccounts[0];
+  //     const c1 = await get(a, counterContract.address);
+  //     if (c1 !== 0n) {
+  //       let result = await reset(a, counterContract.address);
+  //       expect(result.isSuccess()).toBe(true);
+  //     }
+  //     const c2 = await get(a, counterContract.address);
+  //     expect(c2).toBe(0n);
+  //   },
+  //   timeout
+  // );
+
+  // it("creates an account with the session key module", async () => {
+  //   if (!sessionKeyModule) {
+  //     expect(sessionKeyModule).toBeDefined();
+  //     return;
+  //   }
+  //   const conf = config(env);
+  //   const p = provider(conf.providerURL);
+  //   const a = new Multisig(
+  //     p,
+  //     targetAccountConfigs[0].address,
+  //     [targetAccountConfigs[0].privateKey],
+  //     sessionKeyModule
+  //   );
+  //   targetAccounts = [a];
+  // });
+
+  // it(
+  //   "increments the counter with the session key module",
+  //   async () => {
+  //     const a = targetAccounts[0];
+  //     let result = await increment(a, counterContract.address);
+  //     expect(result.isSuccess()).toBe(true);
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "reads the counter",
+  //   async () => {
+  //     const a = testAccounts[0];
+  //     const c = await get(a, counterContract.address);
+  //     expect(c).toBe(1n);
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "create and save the regular Multisig (no module)",
+  //   async () => {
+  //     const conf = config(env);
+  //     const p = provider(conf.providerURL);
+  //     const a = new Multisig(p, targetAccountConfigs[0].address, [
+  //       targetAccountConfigs[0].privateKey,
+  //     ]);
+  //     targetAccounts = [a];
+  //   },
+  //   timeout
+  // );
+
+  // it(
+  //   "removes the module from account",
+  //   async () => {
+  //     const a = targetAccounts[0];
+  //     const c = await remove_module(a, classHash("SessionKeyValidator"));
+  //     expect(c.isSuccess()).toEqual(true);
+  //   },
+  //   timeout
+  // );
+});
