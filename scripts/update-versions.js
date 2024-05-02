9b5f2cc495dabcd885541a4294e5c64796e4a7d0
0xknwn <145777008+0xknwn@users.noreply.github.com>
2024-05-02 14:10:15 +0200

    deploy version 0.1.4 of the SDKs (#123)

---
+++ scripts/update-versions.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+const { argv } = require("node:process");
+
+if (argv.length !== 3) {
+  console.log("Usage: node update-versions.js <version>");
+  process.exit(1);
+}
+
+const version = argv[2];
+
+for (package of [
+  "package.json",
+  "sdks/account/package.json",
+  "sdks/module-sessionkey/package.json",
+]) {
+  const mainPackage = fs.readFileSync(package, "utf-8");
+  const mainPackageJson = JSON.parse(mainPackage);
+  mainPackageJson.version = version;
+  fs.writeFileSync(package, JSON.stringify(mainPackageJson, null, 2));
+}
