48f996b1a5dc03f866012a163635709a0b165ab9
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-01-15 15:12:45 +0100

    fix @0xknwn/starknet-test-helpers tests

--- sdks/__tests__/helpers/src/contract.test.ts
+++ sdks/__tests__/helpers/src/contract.test.ts
@@ -12,7 +12,7 @@ describe("contract management (helper)", () => {
         "0x39d9e6ce352ad4530a0ef5d5a18fd3303c3606a7fa6ac5b620020ad681cc33b";
         const class_hash = classHash("SimpleAccount");
       expect(class_hash).toBe(
-        "0x69e764188a1dd42abc108888c1853913f078052c790c67f31b08a7a31a078e1"
+        "0x1a3f5a334da41fc1eb5940c691f13df75a215967b0ce9b5a78c5cff4e847293"
       );
       const constructorCallData = [
         "0x39d9e6ce352ad4530a0ef5d5a18fd3303c3606a7fa6ac5b620020ad681cc33b",
@@ -26,7 +26,7 @@ describe("contract management (helper)", () => {
         0
       );
       expect(account_address).toBe(
-        "0x573fed1a9584a6ca9d8248bbfd2c6267fa64677452a39821fb33551a4c5d9e7"
+        "0x4a3c9f794dfa1b6a63e344720e4806a04160237b8aa9771e6f76ae9326eca5f"
       );
     },
     default_timeout
