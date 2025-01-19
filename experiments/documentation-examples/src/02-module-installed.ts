2ddcda44c7b1e67a7bcea9f573b622ab670791d2
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-01-19 19:38:37 +0100

    move 02 scripts to protocol v3

--- experiments/documentation-examples/src/02-module-installed.ts
+++ experiments/documentation-examples/src/02-module-installed.ts
@@ -11,7 +11,10 @@ const main = async () => {
   const account = new SmartrAccount(
     provider,
     accountAddress,
-    smartrAccountPrivateKey
+    smartrAccountPrivateKey,
+    undefined,
+    "1",
+    "0x3"
   );
   const isInstalled = await account.isModule(classHash("StarkValidator"));
   console.log(
--- experiments/documentation-examples/src/02-registered-publickey.ts
+++ experiments/documentation-examples/src/02-registered-publickey.ts
@@ -15,7 +15,10 @@ const main = async () => {
   const account = new SmartrAccount(
     provider,
     accountAddress,
-    smartrAccountPrivateKey
+    smartrAccountPrivateKey,
+    undefined,
+    "1",
+    "0x3"
   );
   const moduleCallData = new CallData(StarkValidatorABI);
   const calldata = await moduleCallData.compile("get_public_key", {});
