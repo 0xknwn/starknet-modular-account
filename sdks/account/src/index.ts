e2acb80be51d92912a87ee6fc39ab63f599b1513
0xknwn <145777008+0xknwn@users.noreply.github.com>
2025-01-29 18:07:48 +0100

    publish STRK contract and upgrade to v0.2.5 (#199)

--- sdks/account/src/index.ts
+++ sdks/account/src/index.ts
@@ -4,4 +4,6 @@ export * from "./message";
 export * from "./smartr_account";
 import { ABI as StarkValidatorABI } from "./abi/StarkValidator";
 import { ABI as SmartrAccountABI } from "./abi/SmartrAccount";
-export { StarkValidatorABI, SmartrAccountABI };
+import { ABI as ERC20ABI } from "./abi/ERC20";
+export { StarkValidatorABI, SmartrAccountABI, ERC20ABI };
+export { ETH, STRK } from "./natives";
